d04e16cacd576e2e9c4bd085c0a44416bf4d190b
disuye <dandisuye@gmail.com>
2025-11-28 10:33:40 +0000

    lut script comments

--- demo_all_luts.sh
+++ demo_all_luts.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# This script renders one demo image file through an entire folder of 3DLUT files, in order to quickly compare all LUTs side-by-side...
+# Change the variables: 
+# LUT_DIR = location folder that contains all of your 3DLUT files; can contain subfolders; file names with special characters *might* crash the script
+# OUTPUT_DIR = name & location of the folder where all processed images should be saved
+# SOURCE = name & location of the image file you wish to use, can be almost any image format
+# Do not forget to make this script executable: chmod +x demo_all_luts.sh
+# github.com @disuye
 
 LUT_DIR="3dluts";
 OUTPUT_DIR="LUT_Demo";
-SOURCE="ARTWORK6.dng";
+SOURCE="ARTWORK.dng";
 mkdir -p "${OUTPUT_DIR}_${SOURCE%.*}"; \
 find "${LUT_DIR}/" -type f \( -iname "*.cube" -o -iname "*.3dl" -o -iname "*.lut" \) -print0 | \
 while IFS= read -r -d '' lut; do \
