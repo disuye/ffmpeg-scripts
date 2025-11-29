fbd11b8eab0325b1ca4eef36950a6746243fddb3
disuye <dandisuye@gmail.com>
2025-11-29 17:41:52 +0000

    promo maker v1 done

--- vinyl.sh
+++ vinyl.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
-# FFMPEG script to simulate record artwork on spinning turntable... (turntable spindle is +9x -3y [10.8, 3.6]  pixels [high & to the right] off centre)
+# FFMPEG script to simulate record artwork on spinning turntable... (turntable spindle is +10h -4w pixels [high & to the right] off centre)
 # -filter_complex:
 # Extract luma matte from the background turntable video (lines 1-2-3)
 # Rotate vinyl label artwork at 45RPM, crop using above luma matte (lines 4-5-6)
@@ -8,7 +8,7 @@
 ARTWORK=ARTWORK66.PNG
 BACKGROUND=footage/C0002.MP4
 MUSIC=MUSIC.wav
-ffmpeg -ss 00:00:15 \
+ffmpeg -ss 00:00:25 \
        -i ${BACKGROUND} \
        -loop 1 -i mask.png \
        -loop 1 -i matte.png \
@@ -16,14 +16,14 @@ ffmpeg -ss 00:00:15 \
        -ss 00:04:29 \
        -i ${MUSIC} \
        -t 90 \
-       -filter_complex "[0:v]crop='min(iw,ih)':'min(iw,ih)', scale=1800:1800, format=rgba, split=2[A][B]; \
-              [B]hue=s=0, eq=contrast=500:brightness=0.8, format=rgba[C]; \
+       -filter_complex "[0:v]crop='min(iw,ih)':'min(iw,ih)', pad='min(iw,ih)'+20:'min(iw,ih)'+20:10:20:green, scale=2160:2160, format=rgba, split=2[A][B]; \
+              [B]hue=s=0, eq=contrast=500:brightness=0.8, boxblur=3:1:1, format=rgba[C]; \
               [C][1:v]blend=all_mode=darken, format=rgba[garbage_mask]; \
               [garbage_mask][2:v]blend=all_mode=lighten, hue=s=0, format=rgba[luma_matte]; \
-              [3:v]crop='min(iw,ih)':'min(iw,ih)', scale=1800:1800, format=rgba[artwork]; \
-              [artwork]rotate=2*PI*t*(45/60):c=black, crop=1800:1800:(iw-1800)/2:(ih-1800)/2, scale=1800:1800, format=rgba[label]; \
-              [label][luma_matte]alphamerge, format=rgba[label_masked]; \
-              [A][label_masked]overlay, lut3d='3dluts_vinyl/5.cube', format=rgba[output]" \
+              [3:v]crop='min(iw,ih)':'min(iw,ih)', pad='min(iw,ih)'+2160:'min(iw,ih)'+2160:1080:1080:black, scale=2160:2160, format=rgba[artwork]; \
+              [artwork]rotate=2*PI*t*(45/60):c=black, crop=2160:2160:(iw-2160)/2:(ih-2160)/2, scale=2160:2160, format=rgba[label]; \
+              [label][luma_matte]alphamerge, colorchannelmixer=aa=0.88, format=rgba[label_masked]; \
+              [A][label_masked]overlay, crop=2000:2000:80:80, format=rgba[output]" \
        -map "[output]" -map 4:a \
        -c:v libx264 -pix_fmt yuv420p -r 25 \
        -c:a aac -b:a 320k \
